f5aa60d5338c09571c65586d9bece631e69036fc
byungjun <leebj4690@naver.com>
2024-01-04 16:24:38 +0900

    day0104

---
+++ javaTest/HelloTest.java
@@ -0,0 +1,14 @@
+public class HelloTest { //클래스명은 대문자로 시작
+
+	public static void main(String [] arr) { //main메서드는 자바프로그램의 시작점
+	//main메서드가 없으면 실행불가
+	/*
+	자바주석 : 여러줄을 써도 됩니다
+	여러줄 가능
+	*/
+		System.out.println("나는 오늘 자바수업중"); //println은 다음줄로 넘어감
+		System.out.print("자바 재밌네"); //print는 개행을 하지않음
+		System.out.println("자바 어렵네요");
+		System.out.print("자바클래스와 메서드");
+	}
+}
---
+++ javaTest/JavaStart.java
@@ -0,0 +1,9 @@
+public class JavaStart {
+
+	public static void main(String [] args) { //String 문자열, [] 배열, args 변수
+
+		System.out.println("***Java Start!!!***");
+		System.out.print("안녕 나는 수연샘이야");
+
+	}
+}
